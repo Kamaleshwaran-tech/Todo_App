2745a8ecdce7a134ce654b97532a088e502c5c4e
Kamaleshwaran <kamaleshwaranr2@gmail.com>
2026-07-02 18:54:00 +0530

    Implement the commit and exit button

--- file_interact/gui.py
+++ file_interact/gui.py
@@ -7,14 +7,20 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values = functions.get_todo(),key="todos",
                       enable_events=True , size=(45,10))
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+layout = [[label],[input_box,add_button],[list_box,edit_button,complete_button],
+          [exit_button]]
+
+button_labels = ["Close","Apply"]
+
 
 window = sg.Window('My To-Do App',
-                   layout=[[label],[input_box,add_button],[list_box,edit_button]],
+                   layout=layout,
                    font=("Helvetica", 20))
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED:
-        break
+
     print(event)
     print(values)
     print(values["todos"])
@@ -35,9 +41,17 @@ while True:
             todos[index] = new_todo
             functions.put_todo(todos)
             window['todos'].update(values=todos)
-
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todo()
+            todos.remove(todo_to_complete)
+            functions.put_todo(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value = "")
         case "todos":
             window['todo'].update(value = values['todos'][0])
+        case "Exit":
+            break
         case sg.WIN_CLOSED:
             break
 
